74aab6c715ad205069ec5f509fe5b82c8554fd3f
tis <92219795+this-is-spear@users.noreply.github.com>
2022-07-05 20:29:45 +0900

    init : 스프링 프로젝트 생성

---
+++ src/main/java/fashionable/simba/yanawaserver/YanawaserverApplication.java
@@ -0,0 +1,13 @@
+package fashionable.simba.yanawaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanawaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YanawaserverApplication.class, args);
+	}
+
+}

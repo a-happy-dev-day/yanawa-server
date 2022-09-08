603613f8c32a6dc74ba559ec8bb59e11233d5a4b
tis <92219795+this-is-spear@users.noreply.github.com>
2022-09-08 15:52:36 +0900

    Feat : AccessCode 구현 및 테스트

---
+++ src/test/java/fashionable/simba/yanawaserver/auth/filter/AccessCodeTest.java
@@ -0,0 +1,28 @@
+package fashionable.simba.yanawaserver.auth.filter;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class AccessCodeTest {
+
+    @Test
+    @DisplayName("액세스 코드를 생성한다.")
+    void accessCode_create() {
+        String code = "access-code";
+        AccessCode accessCode = assertDoesNotThrow(
+            () -> new AccessCode(code)
+        );
+
+        assertThat(accessCode).isEqualTo(new AccessCode(code));
+    }
+
+    @Test
+    @DisplayName("동등성 테스트")
+    void accessCode_equals() {
+        EqualsVerifier.forClass(AccessCode.class);
+    }
+}

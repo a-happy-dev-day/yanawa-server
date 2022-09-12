b6bdfaec9294b43a5ea497d81f596ca574b5a63f
tis <92219795+this-is-spear@users.noreply.github.com>
2022-09-12 11:51:15 +0900

    Refact : AuthConfig 값 주입 방식 변경

--- src/main/java/fashionable/simba/yanawaserver/auth/AuthConfig.java
+++ src/main/java/fashionable/simba/yanawaserver/auth/AuthConfig.java
@@ -4,14 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import fashionable.simba.yanawaserver.auth.authorization.AuthenticationPrincipalArgumentResolver;
 import fashionable.simba.yanawaserver.auth.authorization.secured.SecuredAnnotationChecker;
 import fashionable.simba.yanawaserver.auth.context.SecurityContextPersistenceFilter;
-import fashionable.simba.yanawaserver.auth.filter.KakaoTokenAuthenticationFilter;
-import fashionable.simba.yanawaserver.auth.filter.KakaoTokenAuthorizationFilter;
+import fashionable.simba.yanawaserver.auth.filter.ServerTokenAuthenticationFilter;
+import fashionable.simba.yanawaserver.auth.filter.ServerTokenAuthorizationFilter;
 import fashionable.simba.yanawaserver.auth.handler.AuthenticationFailureHandler;
 import fashionable.simba.yanawaserver.auth.handler.AuthenticationSuccessHandler;
 import fashionable.simba.yanawaserver.auth.handler.DefaultAuthenticationFailureHandler;
 import fashionable.simba.yanawaserver.auth.handler.DefaultAuthenticationSuccessHandler;
 import fashionable.simba.yanawaserver.auth.handler.LoginAuthenticationFailureHandler;
 import fashionable.simba.yanawaserver.auth.handler.TokenAuthenticationSuccessHandler;
+import fashionable.simba.yanawaserver.auth.kakao.KakaoAuthenticationClient;
+import fashionable.simba.yanawaserver.auth.kakao.KakaoAuthenticationService;
+import fashionable.simba.yanawaserver.auth.kakao.KakaoAuthorizationClient;
 import fashionable.simba.yanawaserver.auth.provider.AuthenticationManager;
 import fashionable.simba.yanawaserver.auth.provider.JwtTokenProvider;
 import fashionable.simba.yanawaserver.auth.provider.TokenAuthenticationProvider;
@@ -28,21 +31,31 @@ import java.util.List;
 
 @Configuration
 public class AuthConfig implements WebMvcConfigurer {
-    @Value("${security.jwt.token.secret-key}")
-    private String secretKey;
-    @Value("${security.jwt.token.expire-length}")
-    private long validityInMilliseconds;
+    private final String secretKey;
+    private final long validityInMilliseconds;
     private final UserDetailsService userDetailsService;
 
-    public AuthConfig(UserDetailsService userDetailsService) {
+    public AuthConfig(@Value("${security.jwt.token.secret-key}") String secretKey,
+                      @Value("${security.jwt.token.expire-length}") long validityInMilliseconds,
+                      UserDetailsService userDetailsService) {
+        this.secretKey = secretKey;
+        this.validityInMilliseconds = validityInMilliseconds;
         this.userDetailsService = userDetailsService;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new SecurityContextPersistenceFilter());
-        registry.addInterceptor(new KakaoTokenAuthenticationFilter(tokenAuthenticationSuccessHandler(), loginFailureHandler(), userDetailsAuthenticationProvider(), objectMapper())).addPathPatterns("/login/token");
-        registry.addInterceptor(new KakaoTokenAuthorizationFilter(successHandler(), failureHandler(), tokenAuthenticationProvider()));
+        registry.addInterceptor(serverTokenAuthenticationFilter()).addPathPatterns("/login/token");
+        registry.addInterceptor(serverTokenAuthorizationFilter());
+    }
+
+    private ServerTokenAuthorizationFilter serverTokenAuthorizationFilter() {
+        return new ServerTokenAuthorizationFilter(successHandler(), failureHandler(), tokenAuthenticationProvider());
+    }
+
+    private ServerTokenAuthenticationFilter serverTokenAuthenticationFilter() {
+        return new ServerTokenAuthenticationFilter(tokenAuthenticationSuccessHandler(), loginFailureHandler(), userDetailsAuthenticationProvider(), objectMapper());
     }
 
     @Override

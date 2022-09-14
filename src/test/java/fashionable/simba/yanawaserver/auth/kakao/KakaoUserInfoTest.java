1abb3a667d5d64c3e77338def751be67340f40b5
tis <92219795+this-is-spear@users.noreply.github.com>
2022-09-14 22:25:47 +0900

    Test : KakaoUserInfo 테스트

--- src/test/java/fashionable/simba/yanawaserver/auth/kakao/KakaoUserInfoTest.java
+++ src/test/java/fashionable/simba/yanawaserver/auth/kakao/KakaoUserInfoTest.java
@@ -1,24 +1,62 @@
 package fashionable.simba.yanawaserver.auth.kakao;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class KakaoUserInfoTest {
+    Map<String, String> properties;
+    Map<String, Object> kakaoAccount;
+
+    @BeforeEach
+    void setUp() {
+        properties = new HashMap<>();
+        kakaoAccount = new HashMap<>();
+    }
+
     @Test
     @DisplayName("사용자 정보를 조회한다.")
     void userInfo_create() {
-
-        Map<String, String> properties = new HashMap<>();
-        Map<String, Object> kakaoAccount = new HashMap<>();
-
         assertDoesNotThrow(
             () -> new KakaoUserInfo(321L, properties, kakaoAccount)
         );
     }
 
+    @Test
+    @DisplayName("이메일 정보가 없다면 none이 된다.")
+    void userInfo_IfNotExistEmail() {
+        KakaoUserInfo userInfo = new KakaoUserInfo(321L, properties, kakaoAccount);
+
+        assertThat(userInfo.getEmail()).isEqualTo("none");
+    }
+
+    @Test
+    @DisplayName("사용자의 프로퍼티 정보를 가져온다.")
+    void userInfo_getProperties() {
+        String email = "email";
+        String userEmail = "email@email.com";
+
+        properties.put(email, userEmail);
+        KakaoUserInfo userInfo = new KakaoUserInfo(321L, properties, kakaoAccount);
+
+        assertThat(userInfo.getProperties().get(email)).isEqualTo(userEmail);
+    }
+
+    @Test
+    @DisplayName("사용자의 계정 정보를 조회한다.")
+    void userInfo_getKakaoAccount() {
+        String nickname = "nickname";
+        String username = "this-is-spear";
+
+        kakaoAccount.put(nickname, username);
+        KakaoUserInfo userInfo = new KakaoUserInfo(321L, properties, kakaoAccount);
+
+        assertThat(userInfo.getKakaoAccount().get(nickname)).isEqualTo(username);
+    }
 }

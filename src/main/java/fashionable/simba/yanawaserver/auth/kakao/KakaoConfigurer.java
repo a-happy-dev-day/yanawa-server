7a53fa65d5e6a7dde4527fda3a4409ed6956f58e
tis <92219795+this-is-spear@users.noreply.github.com>
2022-09-16 11:50:35 +0900

    Refact : ConfigurationProperties 을 이용한 값 주입으로 수정

--- src/main/java/fashionable/simba/yanawaserver/auth/kakao/KakaoConfigurer.java
+++ src/main/java/fashionable/simba/yanawaserver/auth/kakao/KakaoConfigurer.java
@@ -1,6 +1,5 @@
 package fashionable.simba.yanawaserver.auth.kakao;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,26 +7,25 @@ import org.springframework.context.annotation.Configuration;
 public class KakaoConfigurer {
     private final KakaoAuthorizationClient kakaoAuthorizationClient;
     private final KakaoAuthenticationClient kakaoAuthenticationClient;
-    private final String redirectUri;
-    private final String clientId;
-    private final String secretKey;
+    private final KakaoPropertiesConfigurer kakaoPropertiesConfigurer;
 
-    public KakaoConfigurer(KakaoAuthorizationClient kakaoAuthorizationClient,
-                           KakaoAuthenticationClient kakaoAuthenticationClient,
-                           @Value("${kakao.client.redirect-uri}") String redirectUri,
-                           @Value("${kakao.client.id}") String clientId,
-                           @Value("${kakao.client.secret}") String secretKey) {
+    public KakaoConfigurer(KakaoAuthorizationClient kakaoAuthorizationClient, KakaoAuthenticationClient kakaoAuthenticationClient, KakaoPropertiesConfigurer kakaoPropertiesConfigurer) {
         this.kakaoAuthorizationClient = kakaoAuthorizationClient;
         this.kakaoAuthenticationClient = kakaoAuthenticationClient;
-        this.redirectUri = redirectUri;
-        this.clientId = clientId;
-        this.secretKey = secretKey;
+        this.kakaoPropertiesConfigurer = kakaoPropertiesConfigurer;
     }
 
     @Bean
     KakaoAuthenticationService kakaoAuthenticationService() {
-        return new KakaoAuthenticationService(kakaoAuthenticationClient, kakaoAuthorizationClient, redirectUri, clientId, secretKey);
+        return new KakaoAuthenticationService(
+            kakaoAuthenticationClient,
+            kakaoAuthorizationClient,
+            kakaoPropertiesConfigurer.getRedirectUri(),
+            kakaoPropertiesConfigurer.getClientId(),
+            kakaoPropertiesConfigurer.getSecretKey()
+        );
     }
+
 
 }
 

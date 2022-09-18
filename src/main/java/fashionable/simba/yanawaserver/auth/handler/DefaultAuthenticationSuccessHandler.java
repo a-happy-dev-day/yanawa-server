14f278381828c53124eaa6161f9596c311423bc9
tis <92219795+this-is-spear@users.noreply.github.com>
2022-09-18 15:47:32 +0900

    Refact : DefaultAuthenticationSuccessHandler 성공 상황 로그 출력

--- src/main/java/fashionable/simba/yanawaserver/auth/handler/DefaultAuthenticationSuccessHandler.java
+++ src/main/java/fashionable/simba/yanawaserver/auth/handler/DefaultAuthenticationSuccessHandler.java
@@ -1,13 +1,17 @@
 package fashionable.simba.yanawaserver.auth.handler;
 
 import fashionable.simba.yanawaserver.auth.context.Authentication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class DefaultAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    private static final Logger log = LoggerFactory.getLogger(DefaultAuthenticationSuccessHandler.class);
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-
+        log.debug("Success login username is {} and authorities is {}", authentication.getPrincipal(), authentication.getAuthorities());
     }
 }

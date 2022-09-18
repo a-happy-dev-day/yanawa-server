22aedeec74258992d3840843687691d37886ea4b
tis <92219795+this-is-spear@users.noreply.github.com>
2022-09-18 15:53:36 +0900

    Refact : MemberDocumentation 리팩토링

--- src/test/java/fashionable/simba/yanawaserver/documentation/MemberDocumentation.java
+++ src/test/java/fashionable/simba/yanawaserver/documentation/MemberDocumentation.java
@@ -5,6 +5,7 @@ import fashionable.simba.yanawaserver.members.domain.RoleType;
 import fashionable.simba.yanawaserver.members.service.MemberService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 import java.util.Optional;
@@ -34,6 +35,7 @@ class MemberDocumentation extends Documentation {
                 preprocessResponse(prettyPrint())))
             .when().get("/members/me")
             .then().log().all()
+            .statusCode(HttpStatus.OK.value())
             .extract();
     }
 

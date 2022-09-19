7e198f90744119638f290312afeda1d025d5b10e
tis <92219795+this-is-spear@users.noreply.github.com>
2022-09-19 11:24:51 +0900

    Refact : 로깅 생략

--- src/main/java/fashionable/simba/yanawaserver/auth/ui/LoginController.java
+++ src/main/java/fashionable/simba/yanawaserver/auth/ui/LoginController.java
@@ -60,11 +60,9 @@ public class LoginController {
                                                       @RequestParam(required = false) String error,
                                                       @RequestParam(required = false) String errorDescription) {
         if (error != null) {
-            logDebug(errorDescription, "Login failed cause : {}");
             throw new AccessCodeException("코드를 발급받는 곳에서 문제가 발생했습니다.");
         }
 
-        logDebug(code, "Login success : {}");
         KakaoMember kakaoMember = kakaoAuthenticationService.getUserInfo(kakaoAuthenticationService.getAccessToken(code));
         UserDetails userDetails = userDetailsService.saveKakaoMember(kakaoMember);
         return ResponseEntity.ok(new TokenRequest(jwtTokenProvider.createAuthenticationToken((String) userDetails.getUsername())));
@@ -99,12 +97,5 @@ public class LoginController {
     @Secured(value = {"ROLE_MEMBER", "ROLE_TEST"})
     public ResponseEntity<Void> logout(@AuthenticationPrincipal User user) {
         return ResponseEntity.ok().build();
-    }
-
-    private void logDebug(String message, String format) {
-        if (log.isDebugEnabled()) {
-            String replaceMessage = message.replaceAll("[\n\r\t]", "_");
-            log.debug(format, replaceMessage);
-        }
     }
 }

2968c70cf6365725dd6e7f554ea0578e6c0f99d2
tis <92219795+this-is-spear@users.noreply.github.com>
2022-10-25 21:44:09 +0900

    Feat : InvalidTokenException , InvalidTokenTypeException  추가

---
+++ src/main/java/fashionable/simba/yanawaserver/global/token/exception/InvalidTokenTypeException.java
@@ -0,0 +1,7 @@
+package fashionable.simba.yanawaserver.global.token.exception;
+
+public class InvalidTokenTypeException extends RuntimeException {
+    public InvalidTokenTypeException(String message) {
+        super(message);
+    }
+}

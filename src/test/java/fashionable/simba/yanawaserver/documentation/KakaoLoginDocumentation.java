675dad4abb1f97cd663a4214184f5f8f6fbfc438
tis <92219795+this-is-spear@users.noreply.github.com>
2022-10-25 17:00:26 +0900

    Docs : 카카오 소셜 로그인 API 작성

---
+++ src/test/java/fashionable/simba/yanawaserver/documentation/KakaoLoginDocumentation.java
@@ -0,0 +1,48 @@
+package fashionable.simba.yanawaserver.documentation;
+
+import fashionable.simba.yanawaserver.auth.kakao.KakaoAuthenticationService;
+import fashionable.simba.yanawaserver.auth.kakao.dto.KakaoAccessToken;
+import fashionable.simba.yanawaserver.members.domain.KakaoMember;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
+class KakaoLoginDocumentation extends Documentation {
+    private static final KakaoAccessToken ACCESS_TOKEN = new KakaoAccessToken("Bearer", "accessToken", "refreshToken");
+    private static final KakaoMember KAKAO_MEMBER = new KakaoMember(1L, "tis@email.com", "tis", "tis.img", "tis.jpg", false);
+    @MockBean
+    KakaoAuthenticationService kakaoAuthenticationService;
+
+    @Test
+    void kakaoLogin() {
+        when(kakaoAuthenticationService.getAccessToken(anyString())).thenReturn(ACCESS_TOKEN);
+        when(kakaoAuthenticationService.getUserInfo(ACCESS_TOKEN)).thenReturn(KAKAO_MEMBER);
+
+        Map<String, String> params = new HashMap<>();
+        params.put("accessCode", "insert kakao access code");
+
+        givenNotOauth()
+            .filter(document("login/kakao",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint())))
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(params)
+            .when().post("login/kakao")
+            .then().log().all()
+            .statusCode(HttpStatus.OK.value())
+            .extract();
+        ;
+    }
+}

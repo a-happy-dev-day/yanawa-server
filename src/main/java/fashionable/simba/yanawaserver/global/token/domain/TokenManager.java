421d9bd09aa19f226f8840748dfe4562a43720ab
tis <92219795+this-is-spear@users.noreply.github.com>
2022-10-27 12:02:24 +0900

    Fix : TokenManager 수동 빈 등록 설정

--- src/main/java/fashionable/simba/yanawaserver/global/token/domain/TokenManager.java
+++ src/main/java/fashionable/simba/yanawaserver/global/token/domain/TokenManager.java
@@ -4,9 +4,7 @@ import fashionable.simba.yanawaserver.global.provider.JwtTokenProvider;
 import fashionable.simba.yanawaserver.global.token.exception.InvalidTokenException;
 import fashionable.simba.yanawaserver.global.token.exception.InvalidTokenTypeException;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.stereotype.Service;
 
-@Service
 public class TokenManager {
     private final ValidAccessTokenStorage validAccessTokenRepository;
     private final ValidRefreshTokenStorage validRefreshTokenRepository;
